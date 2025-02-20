6ee084f56708fa92cf83d33b2414fdbd2c27893a
Bas <55401336+BasToTheMax@users.noreply.github.com>
2025-02-20 18:35:05 +0100

    Update and rename const.js to const.example.js

---
+++ const.example.js
@@ -0,0 +1,4 @@
+module.exports = {
+  seed: 'BUH',
+  account: 'nano_123'
+};
--- const.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  seed: '64599B1C7229368E91C1F091C3749A3738FB77A2B7EA888447D16920C571C067',
-  account: 'nano_1pbkbcoqz1piii773g6tuwoyy6gyy7m6yiyqqzokf7yac9d369xcay36gabc'
-};
